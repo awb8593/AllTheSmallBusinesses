9b4f67061b56b74f2c80029b5c9bae1652865957
m-dorsey <dorseymakenzie@gmail.com>
2022-11-19 15:49:08 -0500

    init home page component

--- demo/smallbusiness-ui/src/app/app-routing.module.ts
+++ demo/smallbusiness-ui/src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomePageComponent } from "./home-page/home-page.component";
+
+const routes: Routes = [
+  { path: '', redirectTo: './home-page', pathMatch: 'full' },
+  { path: 'home-page', component: HomePageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
