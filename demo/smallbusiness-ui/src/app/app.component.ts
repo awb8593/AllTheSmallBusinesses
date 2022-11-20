4b0a4ede9e3e9a85d6287eeb70043c276ee5d88d
Adrian Burgos <awb8593@rit.edu>
2022-11-19 20:23:00 -0500

    added missing = sign that was causing location to never change and instead track our ip address; also fixed issue with list never clearing

--- demo/smallbusiness-ui/src/app/app.component.ts
+++ demo/smallbusiness-ui/src/app/app.component.ts
@@ -17,7 +17,7 @@ export class AppComponent {
 
   title = 'smallbusiness-ui';
   private apiURL = 'https://dev.virtualearth.net/REST/v1/LocalSearch/?query=' //coffee&userLocation=47.602038,-122.333964&key=%7BBingMapsKey%7D'
-  private userLocation = '&userLocation'
+  private userLocation = '&userLocation='
   private key = '&key=AnR4Khwxo4KxF35XmhzvhQZghYq8NqhqvB1Akf8kWkOj4c3kzINYy2GbQP1YkSpD'
 
   httpOptions = {
@@ -27,7 +27,11 @@ export class AppComponent {
   constructor(private http: HttpClient) {
   }
 
-  getLocalBusinesses(latitude: number, longitude: number, type: string): void { // Obsercable<Business>[]
+  ngOnInit(): void {
+    this.localCompanies = new Array<Business>()
+  }
+
+  getLocalBusinesses(latitude: number, longitude: number, type: string): void { // Observable<Business>[]
     //return fetch(this.apiURL + type + this.userLocation + latitude + ',' + longitude + this.key)
     //  .then(response => response.json()).then(.json => console.log(json))
     console.log(this.apiURL + type + this.userLocation);
@@ -37,7 +41,6 @@ export class AppComponent {
       var test = JSON.parse(JSON.stringify(info))
       console.log(test.resourceSets[0].resources)
       for(let i = 0; i < test.resourceSets[0].resources.length; i++) {
-        console.log()
         var business = {name: test.resourceSets[0].resources[i].name, address: test.resourceSets[0].resources[i].Address.formattedAddress, phone: test.resourceSets[0].resources[i].PhoneNumber, website: test.resourceSets[0].resources[i].Website}
         this.localCompanies.push(business)
       }
